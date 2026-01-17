3885730a4b52e37aa43707fb6c7aa4311847a2f2
Michael Dressel <michael.dressel@kloenplatz.de>
2026-01-17 21:17:04 +0100

    Add piece numbers to the mappings

--- src/WPmap.hs
+++ src/WPmap.hs
@@ -5,6 +5,7 @@ module WPmap (
     cube
 ) where
 
+import Text.Printf
 import Linear.V3 (V3(V3))
 import Data.List (sortBy)
 
@@ -19,26 +20,27 @@ import Data.List (sortBy)
 data Symbol = A | B | C | D | E
     deriving (Show, Eq, Ord)
 
-type Map = [(V3 Int, Symbol)]
+type Map = [(V3 Int, Int, Symbol)]
 
 type VShape = [V3 Int]
 
 cube :: [VShape] -> Map
-cube = foldl symPiece []
+cube vs = foldl symPiece [] (zip [1..] vs)
 
-symPiece :: Map -> VShape -> Map
-symPiece m vs = zip vs [A,B,C,D,E] ++ m
+symPiece :: Map -> (Int, VShape) -> Map
+symPiece m (i, vs) = zip3 vs (repeat i) [A,B,C,D,E] ++ m
 
 formatMap :: Map -> String
 formatMap m = formatLines 1 (sortBy myord m)
-myord  :: (V3 Int, a) -> (V3 Int, a) -> Ordering
-myord (V3 a b c, _) (V3 d e f, _) = compare c f `mappend`
-                                    compare b e `mappend`
-                                    compare a d
+myord  :: (V3 Int, Int, a) -> (V3 Int, Int, a) -> Ordering
+myord (V3 a b c, _, _) (V3 d e f, _, _) = compare c f `mappend`
+                                          compare b e `mappend`
+                                          compare a d
 
 formatLines :: Int -> Map -> String
-formatLines i ((_,s):xs) = show s ++ sep ++ formatLines (i+1) xs
+formatLines i ((_,j,s):xs) = printf "%2d" j <> "." <> show s <> sep <>
+                formatLines (i+1) xs
             where sep | i `mod` 25 == 0 = "\n\n"
                       | i `mod`  5 == 0 = "\n"
-                      | otherwise = " "
+                      | otherwise = "  "
 formatLines _ [] = "\n"
